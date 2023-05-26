0d886060705715bc639c0c801f53e01cb94a404a
salikbubu <salikbula@gmail.com>
2023-05-26 23:28:14 +0200

    map images

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
 
+// images
+import images from '../src/Data/Images';
+
 function App() {
   return (
     <div className="App">
-      
+      <div className=''>
+        <div className='flex '>
+          {images.map(image => {
+            return (
+              <div className='min-w-[40rem] w-[30rem] p-[40px]'>
+                <img className='w-[100%] h-[100%] rounded-[2rem]' src={image} alt="" />
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/Data/Images.js
@@ -0,0 +1,8 @@
+import image1 from '../Assets/2e9a6c232105d822c7245ee3215d50bca0ff84ca.jpeg';
+import image2 from '../Assets/a-polar-fox-in-greenland-aqqa-rosing-asvid.jpg';
+import image3 from '../Assets/ARCTIC-CHAR.jpg';
+import image4 from '../Assets/Baggrundsbillede.jpg';
+import image5 from '../Assets/Caribou-hunt.jpg';
+import image6 from '../Assets/E50C13D6-9619-4EB0-A424D7F5C1E2AB7B.webp';
+
+export default [image1, image2, image3, image4, image5, image6];
