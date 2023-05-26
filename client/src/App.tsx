3a889e25975f174efa018b8238d5d731174a79f0
salikbubu <salikbula@gmail.com>
2023-05-26 23:57:36 +0200

    Slut

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,46 @@
-import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
 
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
-// images
 import images from "../src/Data/Images";
 import Container from "./Components/Container";
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (carousel.current) {
+      console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }
+  }, []);
+
   return (
     <div className="App">
       <Container>
-        <motion.div className="cursor-grab overflow-hidden bg-slate-400">
-          <motion.div drag="x" className="flex bg-rose-500">
-            {images.map((image) => {
-              return (
-                <motion.div className="min-h-[40rem] min-w-[30rem] p-[40px]">
-                  <img
-                    className="w-[100%] h-[100%] rounded-[2rem] object-cover pointer-events-none"
-                    src={image}
-                    alt=""
-                  />
-                </motion.div>
-              );
-            })}
+        <motion.div
+          ref={carousel}
+          className="cursor-grab overflow-hidden bg-slate-400"
+          whileTap={{cursor: "grabbing"}}
+        >
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -width}}
+            className="flex bg-rose-500"
+          >
+            {images.map((image) => (
+              <motion.div
+                className="min-h-[40rem] min-w-[30rem] p-[40px]"
+                key={image}
+              >
+                <img
+                  className="w-[100%] h-[100%] rounded-[2rem] object-cover pointer-events-none"
+                  src={image}
+                  alt=""
+                />
+              </motion.div>
+            ))}
           </motion.div>
         </motion.div>
       </Container>
