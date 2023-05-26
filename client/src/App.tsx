a27257b04b1053ca4b25023b2e5b3a04ef93334c
salikbubu <salikbula@gmail.com>
2023-05-26 23:40:22 +0200

    slider => drag="x"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,31 @@
-import './App.css';
+import "./App.css";
+
+import {motion} from 'framer-motion'
 
 // images
-import images from '../src/Data/Images';
+import images from "../src/Data/Images";
+import Container from "./Components/Container";
 
 function App() {
   return (
     <div className="App">
-      <div className=''>
-        <div className='flex '>
-          {images.map(image => {
-            return (
-              <div className='min-w-[40rem] w-[30rem] p-[40px]'>
-                <img className='w-[100%] h-[100%] rounded-[2rem]' src={image} alt="" />
-              </div>
-            )
-          })}
-        </div>
-      </div>
+      <Container>
+        <motion.div className="cursor-grab overflow-hidden bg-slate-400">
+          <motion.div drag="x" className="flex bg-rose-500">
+            {images.map((image) => {
+              return (
+                <motion.div className="min-h-[40rem] min-w-[30rem] p-[40px]">
+                  <img
+                    className="w-[100%] h-[100%] rounded-[2rem] object-cover pointer-events-none"
+                    src={image}
+                    alt=""
+                  />
+                </motion.div>
+              );
+            })}
+          </motion.div>
+        </motion.div>
+      </Container>
     </div>
   );
 }
